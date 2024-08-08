ed2a4d40a18a120689a6a05d95266b0d4dd3baad
Kwok To Yu <asdakak6@gmail.com>
2024-08-08 17:23:52 -0400

    add share button ui

--- greenbus_frontend/src/components/SongLists.js
+++ greenbus_frontend/src/components/SongLists.js
@@ -141,8 +141,9 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './SongLists.css'; // Import the CSS file
 import logo from '../assets/logo.png'; // Album logo or cover art
-//import playIcon from '../assets/play.png'; // Placeholder play icon
+import playIcon from '../assets/play.png'; // Placeholder play icon
 import deleteIcon from '../assets/delete.png'; // Placeholder delete icon
+import shareIcon from '../assets/share.png'; // Placeholder share icon
 import axios from 'axios';
 
 import AudioPlayer from './AudioPlayer';
@@ -211,6 +212,22 @@ const SongLists = () => {
       console.error('Failed to delete song', error);
     }
   };
+//share song
+  const handleShareClick = (song) => {
+    const shareData = {
+      title: song.title,
+      text: `Check out this song: ${song.title} by ${song.artist}`,
+      url: window.location.origin + songPath + song.filePath
+    };
+
+    try {
+      navigator.share(shareData)
+        .then(() => console.log('Song shared successfully'))
+        .catch((error) => console.error('Error sharing song', error));
+    } catch (error) {
+      console.error('Share API not supported', error);
+    }
+  };
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -219,10 +236,9 @@ const SongLists = () => {
   const handleSortSelect = (sortOption) => {
     setSelectedSort(sortOption);
     setShowDropdown(false);
-    // Add logic here to sort the songs when the backend functionality is implemented
     fetchSongs({ order: sortOption, keyword: '' });
   };
-//search function
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
     fetchSongs({ order: selectedSort, keyword: event.target.value });
@@ -276,6 +292,9 @@ const SongLists = () => {
                 onPlay={() => handlePlay(index)}
                 ref={(el) => (audioRefs.current[index] = el)}
               />
+              <button className="share-button" onClick={() => handleShareClick(song)}>
+                <img src={shareIcon} alt="Share" />
+              </button>
               <button className="delete-button" onClick={() => handleDeleteClick(song.id)}>
                 <img src={deleteIcon} alt="Delete" />
               </button>
